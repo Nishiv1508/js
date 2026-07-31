e02d167fdbd45786562ff1de76bb313ddf3fe604
Nishiv Trivedi <Nishiv@techeniac.com>
2026-07-31 11:42:43 +0530

    feat: hof implemented

---
+++ hof/script.js
@@ -0,0 +1,17 @@
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const transformer = (str, fn) => {
+  console.log(`OG string: ${str}`);
+  console.log(`Tranformed String: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("Javascript is the best!", upperFirstWord);
+transformer("Javascript is the best!", oneWord);
