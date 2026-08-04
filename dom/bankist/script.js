04b6c9594ef235b7529abe2e6fc6598d4435757f
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-04 19:21:47 +0530

    feat: basic style, creating elements implemented

--- dom/bankist/script.js
+++ dom/bankist/script.js
@@ -29,3 +29,62 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+console.log(document.getElementsByClassName("btn"));
+
+//creating element
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent =
+//   "We use cookies for improved functionality and analytics.";
+message.innerHTML =
+  "We use cookies for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
+header.append(message);
+// header.before(message);
+// header.after(message);
+
+//delete element
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
+
+//styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+// document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// //attributes
+// const logo = document.querySelector(".nav__logo");
+// console.log(logo.alt);
+// console.log(logo.src);
+
+// logo.alt = "Beautiful Minimalistic Logo";
+// console.log(logo.getAttribute("designer"));
+// logo.setAttribute("company", "Bankist");
+
+// const link = document.querySelector(".twitter-link");
+// console.log(link.href);
+// console.log(link.getAttribute("href"));
+
+// //data attributes
+// console.log(logo.dataset.versionNumber);
+
+// //classes
+// logo.classList.add("c");
+// logo.classList.remove("c");
+// logo.classList.toggle("c");
+// logo.classList.contains("c");
