0ebc47fc937ceea106f71088cb68d50172ebbfc0
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-05 10:25:15 +0530

    fix: used modern way of scrolling

--- dom/bankist/script.js
+++ dom/bankist/script.js
@@ -104,8 +104,17 @@ btnScrollTo.addEventListener("click", (e) => {
   );
 
   //scrolling
-  window.scrollTo(
-    s1coords.left + window.pageXOffset,
-    s1coords.top + window.pageYOffset,
-  );
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset,
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  //modern way
+  section1.scrollIntoView({ behavior: "smooth" });
 });
