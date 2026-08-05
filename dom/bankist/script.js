3dcf0e5e516d4522fabe88e2ed5669af5b9a688f
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-05 10:08:22 +0530

    feat: scrolling and cookie-message fixed to bottom position

--- dom/bankist/script.js
+++ dom/bankist/script.js
@@ -88,3 +88,24 @@ message.style.height =
 // logo.classList.remove("c");
 // logo.classList.toggle("c");
 // logo.classList.contains("c");
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", (e) => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log("current scroll (X/Y)", window.scrollX, scrollY);
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth,
+  );
+
+  //scrolling
+  window.scrollTo(
+    s1coords.left + window.pageXOffset,
+    s1coords.top + window.pageYOffset,
+  );
+});
