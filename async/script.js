4e2e5ea0148f67ab99aef1253fd0ca7813ac0554
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-07 16:19:30 +0530

    feat: async-await and promise methods

--- async/script.js
+++ async/script.js
@@ -18,16 +18,16 @@ const cart = ["shoes", "pants", "t-shirt"];
 //Overview:
 const GITHUB_API = "https://api.github.com/users/Nishiv15";
 
-const user = fetch(GITHUB_API); //Readable stream
-console.log(user);
+// const user = fetch(GITHUB_API); //Readable stream
+// console.log(user);
 
-user
-  .then((data) => {
-    return data.json(); //do not forget to return
-  })
-  .then((parsedData) => {
-    console.log(parsedData);
-  });
+// user
+//   .then((data) => {
+//     return data.json(); //do not forget to return
+//   })
+//   .then((parsedData) => {
+//     console.log(parsedData);
+//   });
 
 ////////////////////////////////////////////// CREATING PROMISE (Producer part)
 function createOrder(cart) {
@@ -84,3 +84,71 @@ promise
       "This will must happen as it is after catch block. Catch only handles the then blocks above it",
     );
   });
+
+// async-await
+//async function always return a promise
+
+const p1 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Promise resolved");
+    }, 5000);
+  });
+};
+const p2 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Promise resolved");
+    }, 3000);
+  });
+};
+const val1 = p1();
+const val2 = p2();
+async function handlePromise() {
+  const data = await fetch(GITHUB_API); //fetch returns a promise (readable stream)
+  const parsedData = await data.json();
+  console.log(parsedData);
+
+  const pre1 = await val1;
+  console.log(pre1);
+  const pre2 = await val2;
+  console.log(pre2);
+}
+
+handlePromise();
+
+//Promise methods (time taken will be the time taken by a promise in that array)
+
+//Promise.all([p1,p2,p3]) returns array of results from promises (all promises must get execute is any one is a reject then this will show error)
+
+//Promise.allSettled([p1,p2,p3]) -> same as .all but if one rejects then also it will wait for all promises to get settled.
+
+//Promise.race([p1,p2,p3]) -> fastest promise that is executed and if the promise is rejected then error will be thrown
+
+//Promise.any([p1,p2,p3]) -> Similar like race. waits for 1st promise to get successfull. But if the samallest time promise is rejected then whichever is successfull first, it will return that promise. If all fails then return will be aggregate error (an array of all three errors)
+
+const pro1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Pro1 success"), 3000);
+});
+const pro2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject("Pro2 fail"), 1000);
+});
+const pro3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Pro3 success"), 5000);
+});
+
+// Promise.all([pro1, pro2, pro3])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
+
+// Promise.allSettled([pro1, pro2, pro3])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+// Promise.race([pro1, pro2, pro3])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+Promise.any([pro1, pro2, pro3])
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
